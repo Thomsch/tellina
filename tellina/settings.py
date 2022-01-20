a9873e2bfbd03fa52e8b7b3243d5890063b16b69
Thomas Schweizer <tschweiz@cs.washington.edu>
2022-01-19 21:43:41 -0800

    Move localhost to own statement
    Easier to toggle on and off.

--- tellina/settings.py
+++ tellina/settings.py
@@ -25,9 +25,11 @@ SECRET_KEY = '*zca^nlaejb32n)5@cevdlsb@g3ea4in=x_k1x9we788v&6k5r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kirin.cs.washington.edu', '127.0.0.1', '205.175.118.26', 
+ALLOWED_HOSTS = ['kirin.cs.washington.edu', '205.175.118.26', 
                  '69.91.132.97']
 
+# Localhosts
+ALLOWED_HOSTS.extend(['127.0.0.1','0.0.0.0', 'localhost'])
 
 # Application definition
 
